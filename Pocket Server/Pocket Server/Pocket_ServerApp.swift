bc893bcdef1d597c2bf3e82b36f0cdd860b0a234
Julian Schumacher <Jules.media@outlook.de>
2022-09-18 13:13:26 +0200

    Init Project

---
+++ Pocket Server/Pocket Server/Pocket_ServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pocket_ServerApp.swift
+//  Pocket Server
+//
+//  Created by Julian Schumacher on 18.09.22.
+//
+
+import SwiftUI
+
+@main
+struct Pocket_ServerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
